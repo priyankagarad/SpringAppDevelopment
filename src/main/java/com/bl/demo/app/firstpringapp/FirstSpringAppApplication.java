8b1a420ddee5a234274cc64dd54328c61c9c18b8
priyankagarad <garadpriyanka1995@gmail.com>
2020-06-20 17:42:36 +0530

    ADDED:Welcome Message

--- src/main/java/com/bl/demo/app/firstpringapp/FirstSpringAppApplication.java
+++ src/main/java/com/bl/demo/app/firstpringapp/FirstSpringAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstSpringAppApplication {
     public static void main(String[] args) {
+        System.out.println("Welcome to Spring App development Project");
         SpringApplication.run(FirstSpringAppApplication.class, args);
     }
 }
