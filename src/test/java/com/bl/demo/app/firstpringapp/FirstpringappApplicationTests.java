5ae8613ebe44ccb255f8907e9cde4b0db94757b1
priyankagarad <garadpriyanka1995@gmail.com>
2020-06-20 14:52:39 +0530

    ADDED: Done Configration

---
+++ src/test/java/com/bl/demo/app/firstpringapp/FirstpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.demo.app.firstpringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstpringappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
